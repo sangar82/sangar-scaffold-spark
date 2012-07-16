8a449ad3c911a1f2fd24f1be7ac0d025f14cd381
Iván Sánchez <sangar1982@gmail.com>
2012-07-16 13:15:02 +0200

    Revert "Add files"
    This reverts commit 2a50d4f0adc9c5ac602dc1db99783ceb0cc7ba0e.

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ sangar-scaffold-spark
 =====================
 
 A new way to do scaffolding (works with php-activerecord)
-
-UNDER DEVELOPMENT. THE SPARKS ISN´T YET IN THE SPARKS REPOSITORY
--- config/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$autoload['libraries'] = array('form_validation','sangar_scaffolds', 'session', 'pagination');
-$autoload['language'] = array('form_validation','sangar_scaffolds', 'web');
-$autoload['helper'] = array('url', 'language', 'form', 'language', 'myform');
-
